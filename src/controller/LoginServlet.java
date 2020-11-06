01a6ce456d63d08694b3450d4880b15dfdd7359c
Anzu <anzu2618@yahoo.co.jp>
2020-11-07 01:09:23 +0900

    Loginサーブレット作成

---
+++ src/controller/LoginServlet.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//フォワード
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//入力値の取得
+		String loginId = request.getParameter("login_id");
+		String loginPass = request.getParameter("login_pass");
+		System.out.println(loginId);
+		System.out.println(loginPass);
+	}
+
+}
